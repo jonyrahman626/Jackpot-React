1cd1c448090ceadb79ba3e2cfc066a09b415a463
jonyrahman626 <jonyrahman15@gmail.com>
2021-06-02 15:49:57 +0600

    Functinal Component add

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import AboutBanner from '../components/AboutBanner/AboutBanner';
 import GameDetail from '../components/GameDetail/GameDetail';
 
-class Promotion extends Component {
-    render() {
-        return (
-            <div>
-                <AboutBanner />
-                <GameDetail />
-            </div>
-        );
-    }
+const Promotion = () => {
+    return (
+        <div>
+            <AboutBanner />
+            <GameDetail />
+        </div>
+    );
 }
 
 export default Promotion;
