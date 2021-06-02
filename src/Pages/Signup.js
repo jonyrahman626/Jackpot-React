cde08912bfb85fe3d27134822d58310228e0fe4b
jonyrahman626 <jonyrahman15@gmail.com>
2021-06-02 15:55:54 +0600

    Functinal Component add

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AboutBanner from '../components/AboutBanner/AboutBanner';
 import SignUpForm from '../components/SignUpForm/SignUpForm';
 
-class Promotion extends Component {
-    render() {
-        return (
-            <div>
-                <AboutBanner />
-                <SignUpForm />
-            </div>
-        );
-    }
+const Signup = () => {
+    return (
+        <div>
+            <AboutBanner />
+            <SignUpForm />
+        </div>
+    );
 }
 
-export default Promotion;
+export default Signup;
